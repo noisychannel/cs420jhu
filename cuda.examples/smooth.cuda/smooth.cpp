fd06c62a44689c2b7488e00ab95ddfbdeca6ff96
Randal Burns <randal@cs.jhu.edu>
2012-04-30 16:46:19 -0400

    Final version for course project.

--- cuda.examples/smooth.cuda/smooth.cpp
+++ cuda.examples/smooth.cuda/smooth.cpp
@@ -1,3 +1,17 @@
+/*******************************************************************************
+*
+*    $Id: $
+*
+*    Randal C. Burns
+*    Department of Computer Science
+*    Johns Hopkins University
+*
+*    $Source: $
+*    $Date: $
+*    $Revision: $
+*
+*******************************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,12 +19,38 @@
 #include <assert.h>
 #include <sys/time.h>
 
-
+/*******************************************************************************
+ *  NNSmoother: Nearest Neighbor Smoother
+ *
+ *  This routine implements a simple kernel smoother based on the mean of all 
+ *  points within the kernel.
+ *
+ *  http://en.wikipedia.org/wiki/Kernel_smoother
+ *
+ *  For a grid of width = mxn the smoothed region is only well defined for 
+ *  interior square defined by the corners 
+ *      (width,width) and  (m-width-1,n-width-1) 
+ *  i.e. only for points far enough inside the boundary for which all neighbors
+ *  inside the kernel are defined.
+ *  (There were other options, e.g. make the domain cylic or
+ *   compute only on the available neighbors.  I chose the simplest.)
+ *
+ ********************************************************************************/
 
 /*
+ *  Function:  NNSmother
+ * 
  *  Calculate the smoothed value for an individual point.
- *   This can be where arbitrarily complex algorithms may
- *   be implemented.
+ *   This function could used arbitrarily more complex algorithms.
+ *   We chose the simplest.
+ *
+ *  This functions implements the mean value kernel smoother.
+ *
+ *  Inputs:
+ *     dim -- the dimension the kernel (2*width+1)
+ *     buf -- contains a dim*dim contiguous array of values.
+ *  
+ *  Returns: the mean value
  */
 float NNSmoother ( unsigned dim, float* buf )
 {
@@ -28,6 +68,8 @@ float NNSmoother ( unsigned dim, float* buf )
 }
 
 /*
+ *  Function; cut
+ *
  *  Cut out an array of size (2*width+1)^2 
  *   from inField centered at x,y  and put it into outField
  */
@@ -47,8 +89,13 @@ void cut ( unsigned x, unsigned y, unsigned dim, unsigned width, float* inField,
 
 
 /*
- *  Smooth a field of dimension dim, dim using a kernel of width x width
- *   pVector contains a 2-dim regular array of dim x dim.
+ *  Function: SmoothField
+ *
+ *  Smooth a field of dimension dim, dim using a kernel of width width
+ *  pFieldIn should point to an input array of size dim*dim
+ *  pFieldOut should point to an output array of size dim*dim.
+ *     One should initialize(memset) pFieldOut to 0 so that the border
+ *     do not contain non-zero values
  */
 bool SmoothField ( unsigned dim, unsigned width, float* pFieldIn, float* pFieldOut ) 
 { 
@@ -67,7 +114,13 @@ bool SmoothField ( unsigned dim, unsigned width, float* pFieldIn, float* pFieldO
 }
 
 
-void initField ( unsigned dim, float* pField )
+/*
+ * Function: InitField
+ *
+ *  Initiliaze the input field to well known values for debuggined 
+ *  and visualization purposes.
+ */
+void InitField ( unsigned dim, float* pField )
 {
   for ( unsigned j=0; j<dim; j++ )
   {
@@ -78,25 +131,40 @@ void initField ( unsigned dim, float* pField )
   }
 }
 
+/*
+ * Function: main
+ *
+ *  See inline comments describing steps.
+ */
 int main ()
 {
+  // Set the parameters of the kernel smoother.
+  //  the valid region is from 
+  //   corner (kernelwidth, kernelwidth) to
+  //   corner (dimension-kernelwidth-1, dimension-kernelwidth-1)
   unsigned dimension = 2064;
   unsigned kernelwidth = 8; 
 
-  struct timeval ta, tb;
-
-  gettimeofday ( &ta, NULL );
+//  These are good small values to use.
+//  unsigned dimension = 16;
+//  unsigned kernelwidth = 3; 
 
   // Create the input field
   float *field = (float *) malloc ( dimension * dimension * sizeof(float));
-  initField ( dimension, field );
+  InitField ( dimension, field );
 
   // Create the output field
   float *out = (float *) malloc ( dimension * dimension * sizeof(float));
   memset ( out, 0, dimension * dimension *sizeof(float));
 
+  // Collect timing information
+  struct timeval ta, tb;
+  gettimeofday ( &ta, NULL );
+
+  // Invoke the kernel smoother
   SmoothField ( dimension, kernelwidth, field, out );
 
+  // Report timing information
   gettimeofday ( &tb, NULL );
 
   if ( ta.tv_usec < tb.tv_usec )
@@ -106,8 +174,7 @@ int main ()
     printf ("Elapsed total time (s/m): %d:%d\n", tb.tv_sec - ta.tv_sec - 1, 1000000 - tb.tv_usec + ta.tv_usec );
   }
 
-
-
+  // See what happened.
 //  for ( unsigned j=0; j< dimension; j++ )
 //  {
 //    for ( unsigned i=0; i< dimension; i++ )
@@ -116,5 +183,14 @@ int main ()
 //    }
 //    printf ("\n");
 //  }
-}
 
+}  
+
+
+/*******************************************************************************
+*
+*  Revsion History
+*
+*  $Log: $
+*
+*******************************************************************************/
